4cee4d445ba4caff09a845b47b754eaf68edaaf7
𝕮𝖍𝖆𝖗𝖑𝖎𝖊 ⚡️ <charliewilc0@pm.me>
2026-05-09 22:35:37 +0100

    Configure Astro Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+	site: "https://stoic-observer.charliewilco.workers.dev",
 	output: "static",
+	adapter: cloudflare({
+		imageService: "compile",
+	}),
+	integrations: [sitemap()],
 });
